2e683629c403633ab5529106096c5bd26e1ebd63
Nabil Kazerouni <nabilkaz@gmail.com>
2021-04-21 08:18:59 -0700

    Code split airtable to module

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-const dotenv = require("dotenv");
-const Airtable = require("airtable");
+const airtable = require("./utils/airtable");
 
-dotenv.config();
-Airtable.configure({
-  apiKey: process.env.AIRTABLE_API_KEY,
-});
-
-const base = require("airtable").base("appnB0c3FOT7M66D5");
-
-base("Books")
-  .select({
-    maxRecords: 10,
-    fields: ["Title", "Author", "Completed", "Status"],
-    view: "Main View",
-    filterByFormula: "{Status} = 'Currently Reading'",
-  })
-  .firstPage(function (err, records) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    records.forEach(function (record) {
-      console.log("Retrieved", record.get("Title"));
-    });
-  });
+airtable.get();
---
+++ utils/airtable.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const Airtable = require("airtable");
+Airtable.configure({
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+
+const base = require("airtable").base("appnB0c3FOT7M66D5");
+const get = () => {
+  base("Books")
+    .select({
+      maxRecords: 10,
+      fields: ["Title", "Author", "Completed", "Status"],
+      view: "Main View",
+      filterByFormula: "{Status} = 'Currently Reading'",
+    })
+    .firstPage(function (err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function (record) {
+        console.log("Retrieved", record.get("Title"));
+      });
+    });
+};
+
+module.exports = {
+  get,
+};
