90357be91b8bc1430dc4c90d0b393eb5a3c013ee
Nabil Kazerouni <nabilkaz@gmail.com>
2021-04-22 06:58:42 -0700

    refactor: add base class

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 const airtable = require("./utils/airtable");
+const markdown = require("./utils/markdown");
 
-airtable.get();
+let selectionCriteria = {
+  maxRecords: 10,
+  fields: ["Title", "Author", "Completed", "Status"],
+  view: "Main View",
+  filterByFormula: "{Status} = 'Currently Reading'",
+};
+
+let tableName = "Books";
+
+const recordCallBack = function (err, records) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  records.forEach(function (record) {
+    console.log("Retrieved", record.get("Title"));
+    console.log("Retrieved", record.get("Completed"));
+  });
+};
+
+const table = new airtable.Table(tableName, selectionCriteria);
+table.get(recordCallBack);
